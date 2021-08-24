b0aaa551741eca2dcf3dead2d976aa95c9980210
pedro.silva <pedro.silva@mjv.com.br>
2021-08-24 11:33:53 -0300

    [UPDATE] Added company urls

--- src/app/data/mock-companies.ts
+++ src/app/data/mock-companies.ts
@@ -6,12 +6,12 @@ export const COMPANIES: Company[] = [
         name: 'Golden Tulip',
         logo: './assets/golden-tulip-vector-logo.svg',
         description: 'Os hotéis Golden Tulip oferecem uma perspectiva divertida da vida, do trabalho e das viagens. Alinhados aos comportamentos dos clientes que viajam a trabalho, nossos hotéis incentivam os hóspedes a unir trabalho e lazer permitindo-lhes viver e compartilhar experiências exclusivas e divertidas.',
-        url: '',
+        url: 'golden-tulip/',
     }, {
         id: 2,
         name: 'TOTVS',
         logo: './assets/totvs-icon.svg',
         description: 'Totvs (pronuncia-se tótus) é uma empresa brasileira de software, com sede em São Paulo. A Totvs foi inicialmente formada a partir da fusão das empresas Microsiga e Logocenter. Após a abertura de capital na BOVESPA, em 08/mar/2006,[4] a empresa incorporou seus principais concorrentes nacionais — a RM Sistemas, ainda em 2006, e a Datasul, em 2008.z',
-        url: '',
+        url: 'totvs/',
     }
 ]
